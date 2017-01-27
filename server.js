33897b92f3daf42e3672563569507def2c9a9acc
tushar7debnath <tushar7debnath@live.com>
2017-01-27 23:38:38 +0530

    another test

--- server.js
+++ server.js
@@ -34,6 +34,16 @@ slapp.message('help', ['mention', 'direct_message'], (msg) => {
   msg.say(HELP_TEXT)
 })
 
+
+slapp.event('reaction_added', (msg) => {
+  let token = msg.meta.bot_token
+  let id = msg.body.event.item.ts
+  let channel = msg.body.event.item.channel
+  slapp.client.reactions.add({token, 'smile', id, channel}, (err) => {
+    if (err) console.log('Error adding reaction', err)
+  })
+})
+
 // "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
 slapp
   .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
